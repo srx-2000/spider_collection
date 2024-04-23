54e5fcb398404e0ffbcc5ac1567f1396e25ce3fc
備 <54949399+srx-2000@users.noreply.github.com>
2024-04-23 10:28:05 +0800

    Update zhihu_encrypt.py
    修复issue 30中提出的问题

--- zhihuEncrypt/zhihu_encrypt.py
+++ zhihuEncrypt/zhihu_encrypt.py
@@ -12,7 +12,7 @@ import hashlib
 import random
 
 import requests
-from JsOperation import *
+# from JsOperation import *
 from loguru import logger
 
 # 知乎加密逻辑复现
@@ -150,6 +150,84 @@ h_zb = [20, 223, 245, 7, 248, 2, 194, 209, 87, 6, 227, 253, 240, 128, 222, 91, 2
         136, 206, 135, 175, 195, 25, 92, 121, 208, 126, 139, 3, 75, 141, 21, 130, 98, 241, 40, 154, 66, 184, 49,
         181, 46, 243, 88, 101, 183, 8, 23, 72, 188, 104, 179, 210, 134, 250, 201, 164, 89, 216, 202, 220, 50,
         221, 152, 140, 33, 235, 214]
+import ctypes
+
+
+# 由于python的逻辑运算与js的逻辑运算有一些区别，这里加入一个转换类
+def urs(x, bit):
+    """
+    无符号右移
+    @param x:需要移动的值
+    @param bit:移动的位数
+    @return:
+    """
+    x, y = ctypes.c_uint32(x).value, bit % 32
+    if y == 0:
+        return ctypes.c_uint32(x >> y).value & 0xFFFFFFFF
+    return ctypes.c_uint32(x >> y).value
+
+
+def uls(x, bit):
+    """
+    无符号左移
+    @param x:需要移动的值
+    @param bit:移动的位数
+    @return:
+    """
+    x, y = ctypes.c_uint32(x).value, bit % 32
+    return ctypes.c_uint32(x << y).value
+
+
+def srs(x, bits):
+    """
+    有符号右移
+    @param x:需要移动的值
+    @param bits:移动的位数
+    @return:
+    """
+    # 使用 ctypes 模块将数值强制转换为有符号 32 位整数
+    value = ctypes.c_int32(x).value
+    result = value >> bits
+    return result
+
+
+def sls(x, bits):
+    """
+    有符号左移
+    @param x: 需要移动的
+    @param bits: 移动的位数
+    @return:
+    """
+    # 32 位整数的掩码
+    mask = (1 << 32) - 1
+    # 将 x 视为 32 位有符号整数
+    x = x & mask
+    # 执行有符号左移操作
+    result = (x << bits) & mask
+    # 如果结果大于等于 2^31，则减去 2^32 得到负数
+    if result >= (1 << 31):
+        result -= (1 << 32)
+    return result
+
+
+def band(x, y):
+    """
+    与运算
+    @param x:
+    @param y:
+    @return:
+    """
+    return ctypes.c_int(x & y).value
+
+
+def bxor(x, y):
+    """
+    亦或运算
+    @param x:
+    @param y:
+    @return:
+    """
+    return ctypes.c_int(x ^ y).value
 
 
 class ZhiHuEncrypt:
@@ -312,10 +390,10 @@ class ZhiHuEncrypt:
         encrypted_str = ZhiHuEncrypt.encode(a_v)
         ZhiHuEncrypt.headers.update({"x-zse-96": '2.0_' + encrypted_str})
         ZhiHuEncrypt.headers.update({"cookie": f"d_c0={d_c0};"})
-        ZhiHuEncrypt.headers.update({'accept-encoding': 'gzip, deflate, br',
+        ZhiHuEncrypt.headers.update({'accept-encoding': 'gzip, deflate',
                                      'accept-language': 'zh-CN,zh;q=0.9'})
         res = requests.get(url, headers=ZhiHuEncrypt.headers, timeout=10)
-        logger.info(f"final_result==>{res.json()}")
+        logger.info(f"final_result==>{res.text}")
         return res.json()
 
 
